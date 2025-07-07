964ade5b87d0a392f9c0cc599e973450e1724805
legion112 <ageev.maks2014@gmail.com>
2025-07-07 12:42:35 +0300

    Remove useless assertion

--- src/ValueObject/CardExpiration.php
+++ src/ValueObject/CardExpiration.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace App\ValueObject;
 
-use Symfony\Component\Validator\Constraints as Assert;
-
 final readonly class CardExpiration
 {
     public function __construct(
